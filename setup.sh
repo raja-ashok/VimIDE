fe6bb610a3231b65fd298c385aad3751a22b93cc
raja-ashok <rashok.svks@gmail.com>
2019-03-07 18:50:20 +0530

    Update script to copy cscope tag script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 VIM_ROOT_PATH=~/.vim
 VIMRC=~/.vimrc
 VIMRC_CUR=./vimrc
+CSCOPETAGS=cstags
+HOME_LOC_BIN=~/.local/bin
 
 PATHOGEN_PKG_DIR=./pathogen/autoload
 PATHOGEN_PATH=~/.vim/autoload
@@ -27,12 +29,30 @@ copy_plugins()
     cp ./bundle ${VIM_ROOT_PATH}/ -rf
     cp ./plugins ${VIM_ROOT_PATH}/ -rf
     echo "Copied plugins"
+    ls ./bundle
+}
+
+create_home_loc_bin()
+{
+    if [ ! -d ${HOME_LOC_BIN} ]; then
+        mkdir -p ${HOME_LOC_BIN}
+        echo "PATH=${HOME_LOC_BIN}:\$PATH" >> rc
+        echo "Created ${HOME_LOC_BIN}"
+    fi
+}
+
+copy_cstags_2_bin()
+{
+    cp ${CSCOPETAGS} ${HOME_LOC_BIN}
+    echo "Copied ${CSCOPETAGS} to ${HOME_LOC_BIN}"
 }
 
 do_setup()
 {
+    create_home_loc_bin
     copy_pathogen
     copy_plugins
+    copy_cstags_2_bin
 }
 
 do_setup
