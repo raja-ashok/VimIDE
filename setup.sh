f38980f22ab699047dcf1c1f0edc113661a34e17
raja-ashok <rashok.svks@gmail.com>
2019-06-21 19:36:01 +0530

    Update setup script to install cscope

--- setup.sh
+++ setup.sh
@@ -50,6 +50,7 @@ copy_cstags_2_bin()
 install_required_apt_pkg()
 {
     sudo apt install exuberant-ctags
+    sudo apt install cscope
     sudo apt install vim-gui-common
     sudo apt install vim-runtime
 }
