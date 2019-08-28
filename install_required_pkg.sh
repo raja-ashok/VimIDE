ce96d430348ac87d43cf2ce0af304ba2a3d16f1e
raja-ashok <rashok.svks@gmail.com>
2019-08-28 20:18:52 +0530

    Update install script

--- install_required_pkg.sh
+++ install_required_pkg.sh
@@ -2,9 +2,10 @@
 
 install_required_apt_pkg()
 {
-    sudo apt install exuberant-ctags
-    sudo apt install cscope
-    sudo apt install vim-gui-common
-    sudo apt install vim-runtime
+    apt install exuberant-ctags
+    apt install cscope
+    apt install vim-gui-common
+    apt install vim-runtime
 }
 
+install_required_apt_pkg
